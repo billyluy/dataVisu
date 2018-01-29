cb95c9a7881f8f9fe081cfe1e4742b9a7633256e
bluy <billy.luy@stonybrook.edu>
2018-01-29 09:40:55 -0500

    push hello world

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,7 +18,8 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! ");
+
     }
 
 }
